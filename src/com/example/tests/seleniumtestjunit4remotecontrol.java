3f3f3930a5ecb2927a6cbcfdd77aad9fd20d7e6d
karueglazki <karueglazki@gmail.com>
2015-09-06 09:24:29 +0300

    change webdriver

--- src/com/example/tests/seleniumtestjunit4remotecontrol.java
+++ src/com/example/tests/seleniumtestjunit4remotecontrol.java
@@ -1,17 +1,21 @@
 package com.example.tests;
 
 import com.thoughtworks.selenium.*;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class seleniumtestjunit4remotecontrol {
 	private Selenium selenium;
 
 	@Before
 	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "https://www.google.com.ua/");
-		selenium.start();
+		WebDriver wd = new FirefoxDriver();
+		selenium = new WebDriverBackedSelenium(wd, "https://www.google.com.ua/");
 	}
 
 	@Test
@@ -19,7 +23,7 @@ public class seleniumtestjunit4remotecontrol {
 		selenium.open("");
 		selenium.type("id=lst-ib", "обучение собак");
 		selenium.click("name=btnG");
-		selenium.waitForPageToLoad("3000");
+		selenium.waitForPageToLoad("3");
 	}
 
 	@After
